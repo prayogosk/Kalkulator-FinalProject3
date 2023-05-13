55418be39b543df6ec66a9d4b7f7bf2be9dcc653
Prayogo Sindurojo Kusumojati <108788192+prayogosk@users.noreply.github.com>
2023-05-13 15:33:16 +0700

    Nambahin SplashScreen

--- app/src/main/java/com/prayogosk/finalproject3_kelompok4/MainActivity.java
+++ app/src/main/java/com/prayogosk/finalproject3_kelompok4/MainActivity.java
@@ -1,9 +1,10 @@
 package com.prayogosk.finalproject3_kelompok4;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
